ddf3b745c9a55211fae2e0ddbe37b83c6d369f9e
Cooper Raterink <Cooper Raterink>
2017-01-31 21:51:24 -0600

    Completed lab 2 prep code

--- Lab 2/ADCTestMain.c
+++ Lab 2/ADCTestMain.c
@@ -24,9 +24,11 @@
 
 // input signal connected to PE2/AIN1
 
+#include <stdint.h>
 #include "ADCSWTrigger.h"
 #include "../tm4c123gh6pm.h"
 #include "PLL.h"
+#include "stddef.h"
 
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
@@ -42,6 +44,94 @@ volatile unsigned long ADCvalue;
 // 1.50V    2048
 // 2.25V    3072
 // 3.00V    4095
+
+
+
+void (*PeriodicTask)(void);   // user function
+
+// ***************** TIMER1_Init ****************
+// Activate TIMER1 interrupts to run user task periodically
+// Inputs:  task is a pointer to a user function
+//          period in units (1/clockfreq)
+// Outputs: none
+void Timer1_Init(void(*task)(void), uint32_t period){
+  SYSCTL_RCGCTIMER_R |= 0x02;   // 0) activate TIMER1
+  PeriodicTask = task;          // user function
+  TIMER1_CTL_R = 0x00000000;    // 1) disable TIMER1A during setup
+  TIMER1_CFG_R = 0x00000000;    // 2) configure for 32-bit mode
+  TIMER1_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
+  TIMER1_TAILR_R = period-1;    // 4) reload value
+  TIMER1_TAPR_R = 0;            // 5) bus clock resolution
+  TIMER1_ICR_R = 0x00000001;    // 6) clear TIMER1A timeout flag
+  if(task != NULL) {
+		TIMER1_IMR_R = 0x00000001;    // 7) arm timeout interrupt
+		NVIC_PRI5_R = (NVIC_PRI5_R&0xFFFF00FF)|0x00008000; // 8) priority 4
+// interrupts enabled in the main program after all devices initialized
+// vector number 37, interrupt number 21
+	}
+  NVIC_EN0_R = 1<<21;           // 9) enable IRQ 21 in NVIC
+  TIMER1_CTL_R = 0x00000001;    // 10) enable TIMER1A
+}
+
+
+void Timer1A_Handler(void){
+  TIMER1_ICR_R = TIMER_ICR_TATOCINT;// acknowledge TIMER1A timeout
+  (*PeriodicTask)();                // execute user task
+}
+
+uint32_t ADCtimes[1000];
+uint32_t ADCvals[1000];
+uint32_t logindex = 0;
+uint32_t lastTime = 0;
+uint32_t thisTime = 0;
+void logValues(void){
+	if(logindex < 1000) {
+		thisTime = TIMER1_TAR_R; // get current time
+		ADCtimes[logindex] = thisTime - lastTime; // log time elapsed;
+		ADCvals[logindex] = ADCvalue; // log ADC value
+		
+		lastTime = thisTime; // iterate
+		logindex++;
+	}
+}
+
+// processes collected elapsed time data 
+void processTime(void){
+	uint32_t low = 0xFFFFFFFF, max = 0;
+	
+	for(int i = 0; i < 999; i++) {
+		if(ADCtimes[i] < low) {
+			low = ADCtimes[i];
+		}
+		else if (ADCtimes[i] > max) {
+			max = ADCtimes[i];
+		}
+	}
+	
+	uint32_t timeJitter = max - low;
+}
+
+// processes collected ADC data
+void processData(void){
+	uint32_t low = 0xFFFFFFFF, max = 0;
+	
+	for(int i = 0; i < 1000; i++) {
+		if(ADCvals[i] < low) {
+			low = ADCvals[i];
+		}
+		if(ADCvals[i] > max) {
+			max = ADCvals[i];
+		}
+	}
+	
+	// create probability mass function
+	uint32_t pmf[max - low];
+	for(int i = 0; i < 1000; i++) {
+		pmf[ADCvals[i] - low] += 1;
+	}
+}
+
+_Bool processed = 0;
 int main(void){unsigned long volatile delay;
   PLL_Init();                           // 80 MHz
   ADC0_InitSWTriggerSeq3_Ch1();         // ADC initialization PE2/AIN1
@@ -53,7 +143,18 @@ int main(void){unsigned long volatile delay;
                                         // configure PF2 as GPIO
   GPIO_PORTF_PCTL_R = (GPIO_PORTF_PCTL_R&0xFFFFF0FF)+0x00000000;
   GPIO_PORTF_AMSEL_R = 0;               // disable analog functionality on PF
+	
+	// 3.
+	Timer1_Init(&logValues, 0xFFFFFFFF + 1);
+	
+	// 4.
+	
+	
   while(1){
+		if(logindex >= 1000 && processed == 0) {
+			processData();
+			processed = 1;
+		}
     GPIO_PORTF_DATA_R |= 0x04;          // profile
     ADCvalue = ADC0_InSeq3();
     GPIO_PORTF_DATA_R &= ~0x04;
--- Lab 2/adctestmain.d
+++ Lab 2/adctestmain.d
@@ -1,4 +1,6 @@
 .\adctestmain.o: ADCTestMain.c
+.\adctestmain.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
 .\adctestmain.o: ADCSWTrigger.h
 .\adctestmain.o: ../tm4c123gh6pm.h
 .\adctestmain.o: PLL.h
+.\adctestmain.o: C:\Keil\ARM\ARMCC\bin\..\include\stddef.h
